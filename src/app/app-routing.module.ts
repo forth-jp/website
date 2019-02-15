a7799ab558de4b72a9f881c59af2ed326b451d27
0918nobita <nobita.0918@gmail.com>
2019-02-16 00:02:54 +0900

    :sparkles: サンプルのページを削除し、ルーティングを設定した

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TopComponent } from './components/top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TopComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   declarations: [TopComponent],
