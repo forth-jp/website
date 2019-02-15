476e8c2e39cedf10ec63b87f1fb33110e315de64
0918nobita <nobita.0918@gmail.com>
2019-02-16 00:02:15 +0900

    :sparkles: TopComponent を追加した

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopComponent } from './components/top/top.component';
 
 const routes: Routes = [];
 
 @NgModule({
+  declarations: [TopComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
